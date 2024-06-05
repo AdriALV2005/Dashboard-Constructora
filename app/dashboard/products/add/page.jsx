16469638065c17411d95c124a12f9791e9f84d70
AdriALV2005 <adrimon7221@gmail.com>
2024-06-04 22:16:26 -0500

    adfa

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -5,17 +5,30 @@ const AddProductPage = () => {
   return (
     <div className={styles.container}>
       <form action={addProduct} className={styles.form}>
-        <input type="text" placeholder="title" name="title" required />
-        <select name="cat" id="cat" required>
-          <option value="general">Choose a Category</option>
-          <option value="gato1">Gato 1</option>
-          <option value="gato2">Gato 2</option>
-          <option value="gato3">Gato 3</option>
-        </select>
-        <input type="number" placeholder="price" name="price" />
-        <input type="number" placeholder="stock" name="stock" />
-        <input type="text" placeholder="color" name="color" />
-        <input type="text" placeholder="size" name="size" />
+        <div className={styles.formcompl}>
+          <div className={styles.column}>
+            <h3>hola</h3>
+            <input type="text" placeholder="title" name="title" required />
+            <h3>hola</h3>
+            <select name="cat" id="cat" required>
+              <option value="general">Choose a Category</option>
+              <option value="gato1">Gato 1</option>
+              <option value="gato2">Gato 2</option>
+              <option value="gato3">Gato 3</option>
+            </select>
+            <h3>hola</h3>
+            <input type="number" placeholder="price" name="price" />
+          </div>
+          <div className={styles.column}>
+            <h3>hola</h3>
+            <input type="number" placeholder="stock" name="stock" />
+            <h3>hola</h3>
+            <input type="text" placeholder="color" name="color" />
+            <h3>hola</h3>
+            <input type="text" placeholder="size" name="size" />
+          </div>
+        </div>
+
         <textarea
           name="desc"
           id="desc"
